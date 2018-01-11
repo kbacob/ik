7070e623a16e950a4355dde674e89a90218f66bc
KBACOB <kbacob@mail.ru>
2018-01-11 07:31:14 +0300

    Merging with test app

---
+++ test/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    using System.Threading;
+    using System.Net.Sockets;
+    using ik.Net;
+    using ik.Utils;
+
+
+    class Program
+    {
+        static int intMaxThreads = 32;
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMaxThreads(intMaxThreads, intMaxThreads);
+            ThreadPool.SetMinThreads(2, 2);
+
+            ContentTypes.Init();
+            HTTPServer http = new HTTPServer("any", 80);
+
+            http.AddHttpWorker(@"^hell\S$", Test1);
+            http.AddHttpWorker(@"/", Test2);
+
+            Console.WriteLine(ik.Utils.Environment.GetVariable("TMP"));
+            http.Start();
+            Console.Read();
+            http.Stop();
+
+            ContentTypes.Exit();
+        }
+
+        static string Test1(ClientHeader clientHeader, NetworkStream stream)
+        {
+            Console.WriteLine(clientHeader["ClientIp"]);
+            return "Hello";
+        }
+
+        static string Test2(ClientHeader clientHeader, NetworkStream stream)
+        {
+            Console.WriteLine(clientHeader["ClientIp"]);
+            return ContentTypes.GetContentType("mks");
+        }
+
+    }
+}
